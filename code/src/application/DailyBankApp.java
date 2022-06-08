2194abe229b1f409ee73fa462ba98f6cb5654b05
Nythrix <95078809+aDubayle@users.noreply.github.com>
2022-06-08 14:24:46 +0200

    javadoc

--- code/src/application/DailyBankApp.java
+++ code/src/application/DailyBankApp.java
@@ -2,6 +2,11 @@ package application;
 
 import application.control.DailyBankMainFrame;
 
+/**
+ * Javadoc réaliser par :
+ * @Author Arnaud Dubayle aidé par Lilian Naud
+ */
+
 public class DailyBankApp  {
 
 	public static void main(String[] args) {
